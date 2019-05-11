9238cb1c5e248017450a220c1c3a35e2437da8e7
Yasuto Tanaka <yasuto@ymail.com>
2019-05-11 11:32:30 -0300

    Feat: Add character route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import Home from './routes/Home.html';
 import About from './routes/About.html';
+import Character from './routes/Character.html';
 
 export default {
   '/': Home,
   '/about': About,
+  '/character': Character,
 };
