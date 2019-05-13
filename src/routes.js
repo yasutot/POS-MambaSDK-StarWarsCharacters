0d503b3b6a588ab6622852989d30c8cddb2682a4
Yasuto Tanaka <yasuto@ymail.com>
2019-05-13 09:13:24 -0300

    Adding Characters routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import Home from './routes/Home.html';
-import About from './routes/About.html';
+import Characters from './routes/Characters.html';
 import Character from './routes/Character.html';
 
 export default {
   '/': Home,
-  '/about': About,
+  '/characters': Characters,
   '/character': Character,
 };
