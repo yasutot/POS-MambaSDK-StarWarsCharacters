e2406aaadd2c5d81591a32bf83747e185ea6869b
Yasuto Tanaka <yasuto@ymail.com>
2019-05-13 21:21:38 -0300

    Change Characters route to History, add instructions and minor style changes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import Home from './routes/Home.html';
-import Characters from './routes/Characters.html';
+import History from './routes/History.html';
 import Character from './routes/Character.html';
 
 export default {
   '/': Home,
-  '/characters': Characters,
   '/character': Character,
+  '/history': History,
 };
